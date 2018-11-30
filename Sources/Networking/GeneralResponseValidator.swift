7a6e39a86b9039432536e2efe1e1a282ea869ddb
Nikita Zatsepilov <nzatsepilov@ronasit.com>
2018-11-30 20:41:18 +0600

    Comment code which can break build

--- Sources/Networking/GeneralResponseValidator.swift
+++ Sources/Networking/GeneralResponseValidator.swift
@@ -5,47 +5,48 @@
 
 import Foundation
 
-final class GeneralResponseValidator: ResponseValidator {
-    enum Error: LocalizedError {
-        case wrongResponse
-        case unknown(description: String)
-
-        var errorDescription: String? {
-            switch self {
-            case .wrongResponse:
-                return "Wrong response format"
-
-            case .unknown(let description):
-                return description
-            }
-        }
-    }
-
-    func validate(response: Any) -> Swift.Error? {
-        guard let json = response as? [String: Any], let result = json["result"] as? Bool else {
-            return Error.wrongResponse
-        }
-
-        if result {
-            return nil
-        }
-
-        if let errorTitle = json["error"] as? String, let error = error(fromString: errorTitle) {
-            return error
-        }
-
-        if let errorDescription = json["message"] as? String {
-            return Error.unknown(description: errorDescription)
-        }
-
-        return Error.unknown(description: "An unknown server error occurred.")
-    }
-
-    func error(fromString string: String) -> Error? {
-        switch string {
-        // Convert errors here
-        default:
-            return nil
-        }
-    }
-}
+// FIXME: rewrite
+//final class GeneralResponseValidator: ResponseValidator {
+//    enum Error: LocalizedError {
+//        case wrongResponse
+//        case unknown(description: String)
+//
+//        var errorDescription: String? {
+//            switch self {
+//            case .wrongResponse:
+//                return "Wrong response format"
+//
+//            case .unknown(let description):
+//                return description
+//            }
+//        }
+//    }
+//
+//    func validate(response: Any) -> Swift.Error? {
+//        guard let json = response as? [String: Any], let result = json["result"] as? Bool else {
+//            return Error.wrongResponse
+//        }
+//
+//        if result {
+//            return nil
+//        }
+//
+//        if let errorTitle = json["error"] as? String, let error = error(fromString: errorTitle) {
+//            return error
+//        }
+//
+//        if let errorDescription = json["message"] as? String {
+//            return Error.unknown(description: errorDescription)
+//        }
+//
+//        return Error.unknown(description: "An unknown server error occurred.")
+//    }
+//
+//    func error(fromString string: String) -> Error? {
+//        switch string {
+//        // Convert errors here
+//        default:
+//            return nil
+//        }
+//    }
+//}
--- Sources/Networking/NetworkService.swift
+++ Sources/Networking/NetworkService.swift
@@ -6,137 +6,134 @@
 import Foundation
 import Alamofire
 
-public typealias Failure = (Error) -> Void
-
-open class NetworkService {
-
-    public typealias EncodingCompletionHandler = UploadRequest<GeneralResponse>.EncodingCompletionHandler
-
-    private let sessionManager: Alamofire.SessionManager
-    private let responseBuilder = GeneralResponseBuilder()
-    var responseValidators: [ResponseValidator]?
-    var errorHandlers: [ErrorHandler] = [GeneralErrorHandler()]
-
-    public init(sessionManager: Alamofire.SessionManager = .default) {
-        self.sessionManager = sessionManager
-    }
-
-    @discardableResult
-    public func request(for endpoint: Endpoint,
-                        success: @escaping () -> Void,
-                        failure: @escaping Failure) -> Request<GeneralResponse> {
-        let request = self.request(endpoint: endpoint)
-        request.responseJSON { _, error in
-            if let error = error {
-                failure(error)
-            }
-            else {
-                success()
-            }
-        }
-        return request
-    }
-
-    @discardableResult
-    public func uploadRequest(for endpoint: UploadEndpoint,
-                              encodingCompletion: @escaping EncodingCompletionHandler,
-                              success: @escaping () -> Void,
-                              failure: @escaping Failure) -> UploadRequest<GeneralResponse> {
-        let request = self.uploadRequest(endpoint: endpoint, encodingCompletion: encodingCompletion)
-        request.responseJSON { _, error in
-            if let error = error {
-                failure(error)
-            }
-            else {
-                success()
-            }
-        }
-        return request
-    }
-
-    @discardableResult
-    public func request<T: Decodable>(for endpoint: Endpoint,
-                                      decoder: JSONDecoder = JSONDecoder(),
-                                      success: @escaping (T) -> Void,
-                                      failure: @escaping Failure) -> Request<GeneralResponse> {
-        let request = self.request(endpoint: endpoint)
-        request.responseJSON { response, error in
-            if let error = error {
-                failure(error)
-            }
-            else if let response = response {
-                do {
-                    let parsedResponse: T = try decoder.decode(from: response.jsonData)
-                    success(parsedResponse)
-                }
-                catch {
-                    failure(error)
-                }
-            }
-        }
-        return request
-    }
-
-    @discardableResult
-    public func uploadRequest<T: Decodable>(for endpoint: UploadEndpoint,
-                                            decoder: JSONDecoder = JSONDecoder(),
-                                            encodingCompletion: @escaping EncodingCompletionHandler,
-                                            success: @escaping (T) -> Void,
-                                            failure: @escaping Failure) -> UploadRequest<GeneralResponse> {
-        let request = self.uploadRequest(endpoint: endpoint, encodingCompletion: encodingCompletion)
-        request.responseJSON { response, error in
-            if let error = error {
-                failure(error)
-            }
-            else if let response = response {
-                do {
-                    let parsedResponse: T = try decoder.decode(from: response.jsonData)
-                    success(parsedResponse)
-                }
-                catch {
-                    failure(error)
-                }
-            }
-        }
-        return request
-    }
-
-    // MARK: - Private
-
-    private func request(endpoint: Endpoint) -> Request<GeneralResponse> {
-        let request = Request(endpoint: endpointByAppendingAuthorizationInfo(to: endpoint), responseBuilder: responseBuilder)
-        request.errorHandlers = errorHandlers
-        if let responseValidators = responseValidators {
-            request.responseValidators = responseValidators
-        }
-        return request
-    }
-
-    private func uploadRequest(endpoint: UploadEndpoint,
-                               encodingCompletion: @escaping EncodingCompletionHandler) -> UploadRequest<GeneralResponse> {
-        let request = UploadRequest(endpoint: endpointByAppendingAuthorizationInfo(to: endpoint),
-                                    imageBodyParts: endpoint.imageBodyParts,
-                                    responseBuilder: responseBuilder,
-                                    encodingCompletion: encodingCompletion)
-        request.errorHandlers = errorHandlers
-        if let responseValidators = responseValidators {
-            request.responseValidators = responseValidators
-        }
-        return request
-    }
-
-    private func endpointByAppendingAuthorizationInfo(to endpoint: Endpoint, token: String? = nil) -> Endpoint {
-        return AuthorizedEndpoint.endpoint(source: endpoint, token: token)
-    }
-
-    // This original method implementation was commented because of impossibility to use current session implementation for requets auth
-    //    private func endpointByAppendingAuthorizationInfo(to endpoint: Endpoint) -> Endpoint {
-    //        guard let session = sessionService.session, let token = session.token else {
-    //            return AuthorizedEndpoint.endpoint(source: endpoint, token: AppConfiguration.wildCardToken)
-    //        }
-    //        return AuthorizedEndpoint.endpoint(source: endpoint, token: token)
-    //    }
-}
+//public typealias Failure = (Error) -> Void
+//
+//open class NetworkService {
+//
+//    private let sessionManager: Alamofire.SessionManager
+//
+//    var responseValidators: [ResponseValidator]?
+//    var errorHandlers: [ErrorHandler] = [GeneralErrorHandler()]
+//
+//    public init(sessionManager: Alamofire.SessionManager = .default) {
+//        self.sessionManager = sessionManager
+//    }
+//
+//    @discardableResult
+//    public func request(for endpoint: Endpoint, success: @escaping () -> Void, failure: @escaping Failure) -> Request<GeneralResponse<Result>> {
+//        let request = self.request(endpoint: endpoint)
+//        request.responseJSON { _, error in
+//            if let error = error {
+//                failure(error)
+//            }
+//            else {
+//                success()
+//            }
+//        }
+//        return request
+//    }
+//
+//
+//    @discardableResult
+//    public func uploadRequest<Result>(for endpoint: UploadEndpoint,
+//                              encodingCompletion: @escaping EncodingCompletionHandler<,
+//                              success: @escaping () -> Void,
+//                              failure: @escaping Failure) -> UploadRequest<GeneralResponse> {
+//        let request = self.uploadRequest(endpoint: endpoint, encodingCompletion: encodingCompletion)
+//        request.responseJSON { _, error in
+//            if let error = error {
+//                failure(error)
+//            }
+//            else {
+//                success()
+//            }
+//        }
+//        return request
+//    }
+//
+//    @discardableResult
+//    public func request<T: Decodable>(for endpoint: Endpoint,
+//                                      decoder: JSONDecoder = JSONDecoder(),
+//                                      success: @escaping (T) -> Void,
+//                                      failure: @escaping Failure) -> Request<GeneralResponse> {
+//        let request = self.request(endpoint: endpoint)
+//        request.responseJSON { response, error in
+//            if let error = error {
+//                failure(error)
+//            }
+//            else if let response = response {
+//                do {
+//                    let parsedResponse: T = try decoder.decode(from: response.data)
+//                    success(parsedResponse)
+//                }
+//                catch {
+//                    failure(error)
+//                }
+//            }
+//        }
+//        return request
+//    }
+//
+//    @discardableResult
+//    public func uploadRequest<T: Decodable>(for endpoint: UploadEndpoint,
+//                                            decoder: JSONDecoder = JSONDecoder(),
+//                                            encodingCompletion: @escaping EncodingCompletionHandler,
+//                                            success: @escaping (T) -> Void,
+//                                            failure: @escaping Failure) -> UploadRequest<GeneralResponse> {
+//        let request = self.uploadRequest(endpoint: endpoint, encodingCompletion: encodingCompletion)
+//        request.responseJSON { response, error in
+//            if let error = error {
+//                failure(error)
+//            }
+//            else if let response = response {
+//                do {
+//                    let parsedResponse: T = try decoder.decode(from: response.data)
+//                    success(parsedResponse)
+//                }
+//                catch {
+//                    failure(error)
+//                }
+//            }
+//        }
+//        return request
+//    }
+//
+//    // MARK: - Private
+//
+//    private func request(endpoint: Endpoint) -> Request<GeneralResponse> {
+//        let request = Request(endpoint: endpointByAppendingAuthorizationInfo(to: endpoint), responseBuilder: responseBuilder)
+//        request.errorHandlers = errorHandlers
+//        if let responseValidators = responseValidators {
+//            request.responseValidators = responseValidators
+//        }
+//        return request
+//    }
+//
+//    private func uploadRequest(endpoint: UploadEndpoint,
+//                               encodingCompletion: @escaping EncodingCompletionHandler) -> UploadRequest<GeneralResponse> {
+//        let request = UploadRequest(endpoint: endpointByAppendingAuthorizationInfo(to: endpoint),
+//                                    imageBodyParts: endpoint.imageBodyParts,
+//                                    responseBuilder: responseBuilder,
+//                                    encodingCompletion: encodingCompletion)
+//        request.errorHandlers = errorHandlers
+//        if let responseValidators = responseValidators {
+//            request.responseValidators = responseValidators
+//        }
+//        return request
+//    }
+//
+//    private func endpointByAppendingAuthorizationInfo(to endpoint: Endpoint, token: String? = nil) -> Endpoint {
+//        return AuthorizedEndpoint.endpoint(source: endpoint, token: token)
+//    }
+//
+////     This original method implementation was commented because of impossibility to use current session implementation for requets auth
+////        private func endpointByAppendingAuthorizationInfo(to endpoint: Endpoint) -> Endpoint {
+////            guard let session = sessionService.session, let token = session.token else {
+////                return AuthorizedEndpoint.endpoint(source: endpoint, token: AppConfiguration.wildCardToken)
+////            }
+////            return AuthorizedEndpoint.endpoint(source: endpoint, token: token)
+////        }
+//}
 
 // MARK: Endpoint
 
