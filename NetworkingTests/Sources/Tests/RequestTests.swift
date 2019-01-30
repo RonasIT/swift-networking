31d910694d348ad09cb7648b8bda1d4440ca7303
Nikita Zatsepilov <nzatsepilov@ronasit.com>
2019-01-30 17:23:37 +0600

    Add tests for request results

--- NetworkingTests/Sources/Tests/RequestTests.swift
+++ NetworkingTests/Sources/Tests/RequestTests.swift
@@ -9,11 +9,51 @@ import XCTest
 
 final class RequestTests: XCTestCase {
 
-    var request: CancellableRequest?
+    private var request: CancellableRequest?
 
     override func tearDown() {
         super.tearDown()
         request = nil
+    }
+
+    func testRequestWithDataResult() {
+        testRequestWithDataResult(isTestingUploadRequest: false)
+    }
+
+    func testUploadRequestWithDataResult() {
+        testRequestWithDataResult(isTestingUploadRequest: true)
+    }
+
+    func testRequestWithStringResult() {
+        testRequestWithStringResult(isTestingUploadRequest: false)
+    }
+
+    func testUploadRequestWithStringResult() {
+        testRequestWithStringResult(isTestingUploadRequest: true)
+    }
+
+    func testRequestWithDecodableResult() {
+        testRequestWithDecodableResult(isTestingUploadRequest: false)
+    }
+
+    func testUploadRequestWithDecodableResult() {
+        testRequestWithDecodableResult(isTestingUploadRequest: true)
+    }
+
+    func testRequestWithJSONResult() {
+        testRequestWithJSONResult(isTestingUploadRequest: false)
+    }
+
+    func testUploadRequestWithJSONResult() {
+        testRequestWithJSONResult(isTestingUploadRequest: true)
+    }
+
+    func testRequestWithEmptyResult() {
+        testRequestWithEmptyResult(isTestingUploadRequest: false)
+    }
+
+    func testUploadRequestWithEmptyResult() {
+        testRequestWithEmptyResult(isTestingUploadRequest: true)
     }
 
     func testCancellation() {
@@ -90,5 +130,143 @@ final class RequestTests: XCTestCase {
         _ = XCTWaiter.wait(for: [lifecycleExpectation], timeout: 3)
         XCTAssertNil(request)
         lifecycleExpectation.fulfill()
+    }
+
+    // MARK: - Private
+
+    private func testRequestWithDataResult(isTestingUploadRequest: Bool) {
+        let service = MockNetworkService()
+        let expectedResult = "result".data(using: .utf8)!
+        let endpoint = MockEndpoint(result: expectedResult)
+        let expectation = self.expectation(description: "Expecting same result")
+        expectation.assertForOverFulfill = true
+        func validate(_ result: Data) {
+            XCTAssertEqual(result, expectedResult)
+            expectation.fulfill()
+        }
+        if isTestingUploadRequest {
+            request = service.uploadRequest(for: endpoint, success: { validate($0) }, failure: { _ in
+                XCTFail("Invalid case")
+            })
+        } else {
+            request = service.request(for: endpoint, success: { validate($0) }, failure: { _ in
+                XCTFail("Invalid case")
+            })
+        }
+        wait(for: [expectation], timeout: 5)
+    }
+
+    private func testRequestWithStringResult(isTestingUploadRequest: Bool) {
+        let service = MockNetworkService()
+        let expectedResult = "result"
+        let endpoint = MockEndpoint(result: expectedResult)
+        let expectation = self.expectation(description: "Expecting same result")
+        expectation.assertForOverFulfill = true
+        func validate(_ result: String) {
+            XCTAssertEqual(result, expectedResult)
+            expectation.fulfill()
+        }
+        if isTestingUploadRequest {
+            request = service.uploadRequest(for: endpoint, success: { validate($0) }, failure: { _ in
+                XCTFail("Invalid case")
+            })
+        } else {
+            request = service.request(for: endpoint, success: { validate($0) }, failure: { _ in
+                XCTFail("Invalid case")
+            })
+        }
+        wait(for: [expectation], timeout: 5)
+    }
+
+    private func testRequestWithDecodableResult(isTestingUploadRequest: Bool) {
+        struct User: Equatable, Codable {
+            let firstName: String
+            let lastName: String
+            let birthDate: Date
+        }
+        let service = MockNetworkService()
+        let expectedResult = User(firstName: "John", lastName: "Doe", birthDate: Date())
+        let endpoint = MockEndpoint(result: expectedResult)
+        let expectation = self.expectation(description: "Expecting same result")
+        expectation.assertForOverFulfill = true
+        func validate(_ result: User) {
+            XCTAssertEqual(result, expectedResult)
+            expectation.fulfill()
+        }
+        if isTestingUploadRequest {
+            request = service.uploadRequest(for: endpoint, success: { validate($0) }, failure: { _ in
+                XCTFail("Invalid case")
+            })
+        } else {
+            request = service.request(for: endpoint, success: { validate($0) }, failure: { _ in
+                XCTFail("Invalid case")
+            })
+        }
+        wait(for: [expectation], timeout: 5)
+    }
+
+    private func testRequestWithJSONResult(isTestingUploadRequest: Bool) {
+        let networkService = MockNetworkService()
+        let expectedResult: [String: String] = [
+            "firstName": "John",
+            "lastName": "Doe"
+        ]
+
+        // We want to test `[String: Any]` response with JSONSerialization
+        // We need pass expected result as `[String: Any]`,
+        // because `[String: String]` is `Codable` type
+        let endpoint = MockEndpoint(result: expectedResult as [String: Any])
+        let expectation = self.expectation(description: "Expecting same result")
+        expectation.assertForOverFulfill = true
+        func validate(_ result: [String: String]) {
+            XCTAssertEqual(result, expectedResult)
+            expectation.fulfill()
+        }
+        if isTestingUploadRequest {
+            request = networkService.uploadRequest(for: endpoint, success: { (result: [String: Any]) in
+                // Cast back to `[String: String]` to validate with expected result
+                guard let result = result as? [String: String] else {
+                    XCTFail("Unexpected result")
+                    return
+                }
+                validate(result)
+            }, failure: { error in
+                XCTFail("Invalid case")
+            })
+        } else {
+            request = networkService.request(for: endpoint, success: { (result: [String: Any]) in
+                // Cast back to `[String: String]` to validate with expected result
+                guard let result = result as? [String: String] else {
+                    XCTFail("Unexpected result")
+                    return
+                }
+                validate(result)
+            }, failure: { error in
+                XCTFail("Invalid case")
+            })
+        }
+        wait(for: [expectation], timeout: 5)
+    }
+
+    private func testRequestWithEmptyResult(isTestingUploadRequest: Bool) {
+        let service = MockNetworkService()
+        let endpoint = MockEndpoint()
+        let expectation = self.expectation(description: "Expecting success")
+        expectation.assertForOverFulfill = true
+
+        if isTestingUploadRequest {
+            request = service.uploadRequest(
+                for: endpoint,
+                success: { expectation.fulfill() },
+                failure: { _ in XCTFail("Invalid case")}
+            )
+        } else {
+            request = service.request(
+                for: endpoint,
+                success: { expectation.fulfill() },
+                failure: { _ in XCTFail("Invalid case")}
+            )
+        }
+        wait(for: [expectation], timeout: 5)
     }
 }
