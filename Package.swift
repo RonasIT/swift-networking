ae7eece95f26d58cf61175465efc31b933b506ea
Nikita Zatsepilov <nzatsepilov@ronasit.com>
2020-09-18 19:37:23 +0600

    Fix warnings in Package.swift

--- Package.swift
+++ Package.swift
@@ -6,22 +6,9 @@ import PackageDescription
 let package = Package(
     name: "Networking",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Networking",
-            targets: ["Networking"]
-        ),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .library(name: "Networking", targets: ["Networking"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Networking",
-            path: "Sources"
-        )
+        .target(name: "Networking", path: "Sources")
     ]
 )
